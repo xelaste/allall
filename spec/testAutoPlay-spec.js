df0468ad13cffd0136cdcaea055f71216a38814e
Alexander Stekolchik <ALEXANS@amdocs.com>
2018-09-03 16:31:44 +0300

    .

--- spec/testAutoPlay-spec.js
+++ spec/testAutoPlay-spec.js
@@ -1,9 +1,17 @@
-import AutoPlay from "../src/auto/AutoPlay"
+import AutoPlay from "../src/auto/autoplay"
 
 describe('AutoPlay tests', function () {
     it('AutoPlay', function () {
         let autoPlay = new AutoPlay();
         console.log("$$$$" + autoPlay.getInitialSet().length);
-        expect(autoPlay.getInitialSet()).toBeDefined()
+        expect(autoPlay.getInitialSet()).toBeDefined();
+        let respond = autoPlay.calculate([1,2,3,4],[1,2,3,4]);
+        expect(respond.matches).toEqual(4)
+        expect(respond.exist).toEqual(0)
+        respond = autoPlay.calculate([1,2,3,4],[9,5,6,1]);
+        expect(respond.matches).toEqual(0);
+        expect(respond.exist).toEqual(1)
+        autoPlay.game([1,2,3,4]);
+        autoPlay.game([5,2,6,9]);
     });
 });
--- src/auto/AutoPlay.js
+++ src/auto/AutoPlay.js
@@ -8,7 +8,7 @@ export default class AutoPlay {
                     for (let l = 1; l < 10; l++) {
                         if (i != j && i != k && i != l && j != k && j != l && k != l) {
                             let x = l + k * 10 + j * 100 + i * 1000
-                            this.initialSet.push(x);
+                            this.initialSet.push([i,j,k,l]);
                         }
                     }
                 }
@@ -18,6 +18,51 @@ export default class AutoPlay {
     getInitialSet() {
         return this.initialSet;
     }
+    calculate(secret, guess) 
+    {
+        let exist = 0;
+        let matches = 0
+        for (let i = 0; i < guess.length; i++) 
+        {
+            for (let j = 0; j < secret.length; j++) {
+                if (secret[j] === guess[i]) {
+                    if (i === j) {
+                        matches++;
+                    }
+                    else {
+                        exist++;
+                    }
+                }
+            }
+        }
+        return { matches: matches, exist: exist };
+    }
+    prune(heap, filter, guess) {
+        let result = [];
+        for (let i = 0; i < heap.length; i++) 
+        {
+            let num=heap[i];
+            let response = this.calculate(num, guess);
+            if (response.matches === filter.matches && response.exist === filter.exist) 
+            {
+                result.push(heap[i]);
+            }
+        }
+        return result;
+    }
 
+    game ( secret )
+    {
+        let heap=this.getInitialSet();
+        let i=0;
+       while(heap.length>1)
+        {
+            let num = heap[Math.floor(Math.random() * heap.length)];
+            let filter = this.calculate(secret, num)
+            heap=this.prune(heap,filter,num);
+            i++;
+        }     
+        console.log(i + " @####@")
+    }
 }
 
