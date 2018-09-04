3f6b3e430ba5a8d14a00e92b5bc56d1777fc40ad
Alexander Stekolchik <ALEXANS@amdocs.com>
2018-09-04 16:03:57 +0300

    ,,

--- src/auto/autoplay.js
+++ src/auto/autoplay.js
@@ -6,8 +6,8 @@ export default class AutoPlay {
             for (let j = 1; j < 10; j++) {
                 for (let k = 1; k < 10; k++) {
                     for (let l = 1; l < 10; l++) {
-                        if (i != j && i != k && i != l && j != k && j != l && k != l) {
-                            let x = l + k * 10 + j * 100 + i * 1000
+                        if (i != j && i != k && i != l && j != k && j != l && k != l) 
+                        {
                             this.initialSet.push([i,j,k,l]);
                         }
                     }
